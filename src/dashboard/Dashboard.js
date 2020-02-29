07aa2216877da367042242c38878570b9fb64377
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-02-29 10:37:18 +0100

    Add material-ui/core dependency

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import {makeStyles} from '@material-ui/core/styles';
-import Paper from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
