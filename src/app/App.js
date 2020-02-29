87cf133a5d8566fcb37dd3f77dc39d6fb6441000
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-02-29 10:20:53 +0100

    Create Dashboard.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dashboard from "../dashboard/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Dashboard/>
+        </div>
+    );
 }
 
 export default App;
