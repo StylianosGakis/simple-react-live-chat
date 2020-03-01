9ec4767dffc239db0d75bbac80d38eef94bac853
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-01 10:55:27 +0100

    Finish basic layout of the four components

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -3,6 +3,12 @@ import React from "react";
 import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,6 +17,7 @@ const useStyles = makeStyles((theme) => ({
     },
     flex: {
         display: "flex",
+        alignItems: "center",
     },
     topicsWindow: {
         width: "30%",
@@ -20,6 +27,7 @@ const useStyles = makeStyles((theme) => ({
     chatWindow: {
         width: "70%",
         height: "300px",
+        padding: "20px",
     },
     chatBox: {
         width: "85%",
@@ -35,22 +43,45 @@ function Dashboard() {
     return (
         <div>
             <Paper className={classes.root}>
-                <Typography variant="h4" component="h4">
+                <Typography variant={"h4"} component={"h4"}>
                     Chat app.
                 </Typography>
-                <Typography variant="h5" component="h5">
+                <Typography variant={"h5"} component={"h5"}>
                     Topic placeholder
                 </Typography>
                 <div className={classes.flex}>
                     <div className={classes.topicsWindow}>
-
+                        <List>
+                            {
+                                ["topic"].map(topic => (
+                                    <ListItem button key={topic}>
+                                        <ListItemText primary={topic}/>
+                                    </ListItem>
+                                ))
+                            }
+                        </List>
                     </div>
                     <div className={classes.chatWindow}>
-
+                        {
+                            [{from: "user", msg: "hello"}].map((chat, index) => (
+                                <div className={classes.flex} key={index}>
+                                    <Chip className={classes.chip} label={chat.from}/>
+                                    <Typography variant={"p"}>
+                                        {chat.msg}
+                                    </Typography>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
                 <div className={classes.flex}>
-
+                    <TextField
+                        label="Send a chat"
+                        className={classes.chatBox}
+                    />
+                    <Button variant="contained" color="primary">
+                        Send
+                    </Button>
                 </div>
             </Paper>
         </div>
